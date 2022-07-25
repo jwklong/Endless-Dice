a1bdb498d4d133b48b1ada5ffec33f3821247d6b
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-07-25 17:21:05 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -460,7 +460,7 @@ function updateGridDices(id) {
         if (d.pick.includes(i)) g.className += " picked"
 
         if (gd) {
-            if (gd.type != "normal" || gd.type != "test") g.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
+            if (gd.type != "normal" && gd.type != "test") g.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
             if (gd.value < 10) g.innerHTML += `<img draggable="false" src='images/D${gd.value}.png'>`
         }
     }
@@ -478,7 +478,7 @@ function updateGridDices(id) {
             pdiv.innerHTML = "<div>"+(gd ? gd.value > 9 ? gd.value : "" : "")+"</div>"
             pdiv.className = "picked_dice "+(gd ? gd.type : "empty")
 
-            if (gd.type != "normal" || gd.type != "test") pdiv.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
+            if (gd.type != "normal" && gd.type != "test") pdiv.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
             if (gd.value < 10) pdiv.innerHTML += `<img draggable="false" src='images/D${gd.value}.png'>`
         }
     }
@@ -501,7 +501,7 @@ function spawnRandomDice(id,update=false) {
     var pos = s[Math.floor(Math.random()*s.length)]
     var tp = Math.floor(Math.random()*4)
 
-    grid[pos] = {pos: pos, value: randomInt(d.min_s,d.max_s), type: ["normal","attack","heal","test"][tp], energy: [1,2,2][tp]}
+    grid[pos] = {pos: pos, value: randomInt(d.min_s,d.max_s), type: ["normal","attack","heal","test"][tp], energy: [1,2,2,3][tp]}
 
     if (d.cards.includes('d7') && Math.random() < .15) { // 
         grid[pos].type = "scrambler"
